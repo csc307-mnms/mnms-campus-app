068611d4a9b0a2d2d59bb9654271ebdf6abef3a3
misakitanabe <misaki.a.tanabe@gmail.com>
2024-02-27 12:46:40 -0800

    Updated headerBar, made changePass page section

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -1,25 +1,15 @@
-import PageHeader from "./components/PageHeader.js";
-import Button from "./components/Button.js";
-import NavBar from "./components/NavBar.js";
-import { SectionID } from "./data/data.js";
+// import Button from "./components/Button.js";
+// import NavBar from "./components/NavBar.js";
+import ChangePass from "./sections/ChangePass.js";
+// import { SectionID } from "./data/data.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <PageHeader text="Login" />
-      <p className="text-red-500">Hello World</p>
+      <ChangePass />
 
-      {/* Hello it's me misaki practicing  pushing */}
-
-      <Button
-        text="Sign In"
-        onClick={() => {
-          window.location.href = `#${SectionID.Schedules}`;
-        }}
-        className="bg-[#003831] text-white font-bold py-1 px-32 rounded"
-      />
-      <NavBar activePage="schedules" />
+      {/* <NavBar activePage="schedules" /> */}
     </div>
   );
 }
