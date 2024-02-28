b1e0a6e415c847fe99aa460197927489a380aaf5
Meyli Jaeger <mjaege02@calpoly.edu>
2024-02-27 21:23:11 -0800

    homepage.js done

--- packages/react-frontend/src/sections/homepage.js
+++ packages/react-frontend/src/sections/homepage.js
@@ -1,4 +1,4 @@
-//homepage.js
+// homepage.js
 import React from "react";
 import backgroundImage from "../resources/calpolycampus.jpg";
 import CPlogo from "../resources/calpolylogo.png";
@@ -6,98 +6,61 @@ import Button from "../components/Button.js";
 import TextBox from "../components/TextBox.js";
 
 function HomePage() {
-  const containerStyle = {
-    position: "relative",
-    height: "100vh",
-  };
-
-  const backgroundImageStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    opacity: 0.3,
-    position: "absolute",
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-  };
-
-  const logoStyle = {
-    width: "250px",
-    height: "auto",
-    position: "absolute",
-    top: "40%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-  };
-
-  const textStyle = {
-    position: "absolute",
-    top: "10%",
-    left: "50%",
-    transform: "translateX(-50%)",
-    fontFamily: "sans-serif",
-    color: "#154734",
-    fontSize: "40px",
-    textAlign: "center",
-  };
-
-  const textBoxContainerStyle = {
-    position: "absolute",
-    top: "55%",
-    left: "50%",
-    transform: "translateX(-50%)",
-    display: "flex",
-    flexDirection: "column",
-    gap: "10px",
-    width: "300px",
-    fontSize: "14px",
-  };
-
-  const forgotPasswordButtonStyle = {
-    color: "#0085FF",
-    cursor: "pointer",
-    alignSelf: "flex-end",
-    marginTop: "8px",
-    fontWeight: "bold",
-  };
-
-  const signUpButtonStyle = {
-    color: "#000000",
-    cursor: "pointer",
-    alignSelf: "flex-end",
-    marginTop: "8px",
-    fontWeight: "bold",
-  };
-
   return (
-    <div style={containerStyle}>
-      <div style={backgroundImageStyle}></div>
-      <img src={CPlogo} alt="Logo" style={logoStyle} />
-      <div style={textStyle}>
-        <p>Welcome to PolyBuddy</p>
+    <div className="relative h-screen">
+      <div
+        className="absolute top-0 right-0 bottom-0 left-0"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          opacity: 0.3,
+        }}
+      ></div>
+
+      <img
+        src={CPlogo}
+        alt="Logo"
+        className="w-64 h-auto absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/4"
+      />
+
+      <div
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2"
+        style={{ top: "8%" }}
+      >
+        <p className="font-sans text-green-800 text-center text-5xl">
+          Welcome to PolyBuddy
+        </p>
       </div>
 
-      <div style={textBoxContainerStyle}>
+      <div className="absolute bottom-[110px] left-1/2 transform -translate-x-1/2">
         <TextBox
           text="Username"
           type="text"
-          className="mx-auto w-full mt-12 mb-1"
+          className="mx-auto w-80 mt-12 mb-1"
         />
         <TextBox
           text="Password"
           type="password"
-          className="mx-auto w-full mb-1"
+          className="mx-auto w-80 mt-5 mb-1"
         />
-        <div style={forgotPasswordButtonStyle}>
+
+        <div
+          style={{ marginLeft: "180px" }}
+          className="text-blue-500 cursor-pointer self-end mt-2 font-bold"
+        >
           <p>Forgot Password?</p>
         </div>
+
         <Button
           text="Sign In"
-          className="bg-[#003831] text-white font-bold py-1 pt-2 pb-2 px-24 shadow-xl rounded mt-3"
+          className="bg-[#003831] text-white font-bold py-2 px-24 shadow-xl rounded mt-3 w-80"
         />
-        <div style={signUpButtonStyle}>
+
+        <div
+          style={{ marginLeft: "110px" }}
+          className="text-black cursor-pointer self-end mt-2 font-bold"
+        >
           <p>Need an account? Sign up!</p>
         </div>
       </div>
