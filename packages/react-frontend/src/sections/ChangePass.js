dc756632d4412c1e2720f79ef594447660a91614
misakitanabe <misaki.a.tanabe@gmail.com>
2024-02-27 16:17:09 -0800

    updates changepass page to use textbox component

--- packages/react-frontend/src/sections/ChangePass.js
+++ packages/react-frontend/src/sections/ChangePass.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../components/Button.js";
 import LogoutHeader from "../components/LogoutHeader.js";
 import NavBar from "../components/NavBar.js";
+import TextBox from "../components/TextBox.js";
 
 function ChangePass() {
   return (
@@ -10,42 +11,30 @@ function ChangePass() {
 
       <form>
         {/* Current Password box */}
-        <div className="mb-8 mt-20 mx-auto w-3/4">
-          <label htmlFor="name" className="block font-bold text-left">
-            Current Password
-          </label>
-          <input
-            type="text"
-            className="bg-[#e5e7eb] rounded w-full shadow-lg h-10 mt-2"
-          />
-        </div>
+        <TextBox
+          text="Current Password"
+          type="text"
+          className="mx-auto w-3/4 mt-12 mb-8"
+        />
 
         {/* New Password box */}
-        <div className="mb-8 mx-auto w-3/4">
-          <label htmlFor="job" className="block font-bold text-left">
-            New Password
-          </label>
-          <input
-            type="text"
-            className="bg-[#e5e7eb] rounded w-full shadow-lg h-10 mt-2"
-          />
-        </div>
+        <TextBox
+          text="New Password"
+          type="password"
+          className="mx-auto w-3/4 mb-8"
+        />
 
         {/* Confirm Password box */}
-        <div className="mb-28 mx-auto w-3/4">
-          <label htmlFor="job" className="block font-bold text-left">
-            Confirm Password
-          </label>
-          <input
-            type="text"
-            className="bg-[#e5e7eb] rounded w-full shadow-lg h-10 mt-2"
-          />
-        </div>
+        <TextBox
+          text="Confirm Password"
+          type="password"
+          className="mb-24 mx-auto w-3/4"
+        />
 
         {/* Change Password Button */}
         <Button
           text="Change Password"
-          className="bg-[#003831] text-white font-bold py-1 pt-2 pb-2 px-24 shadow-xl rounded mb-40"
+          className="bg-[#003831] text-white font-bold py-1 pt-2 pb-2 px-24 shadow-xl rounded mb-64"
         />
       </form>
 
