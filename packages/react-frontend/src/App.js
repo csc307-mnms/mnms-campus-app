51c74c78d469026c7aedc822082ae02028cd89e6
nathanip <ipfamilynip@gmail.com>
2024-02-28 14:19:38 -0800

    map first iteration

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -1,12 +1,14 @@
 import HomePage from "./sections/homepage.js";
 import Schedules from "./sections/schedules.js";
 import ChangePass from "./sections/ChangePass.js";
+import Map from "./sections/Map.js";
 import { SectionID } from "./data/data.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import useToken from "./hooks/useToken.js";
 
 function App() {
   const { token, setToken } = useToken();
+  console.log("Token:", token);
 
   if (!token) {
     return <HomePage setToken={setToken} />;
@@ -18,6 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Schedules />} />
           <Route path={`/${SectionID.ChangePass}`} element={<ChangePass />} />
+          <Route path={`/${SectionID.Map}`} element={<Map />} />
         </Routes>
       </BrowserRouter>
     </div>
