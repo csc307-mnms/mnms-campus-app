32b8358406cda988a626f56d9e8c6f9f55f04f2d
Meyli Jaeger <mjaege02@calpoly.edu>
2024-02-28 18:11:02 -0800

    homeChangePass

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -2,6 +2,7 @@ import HomePage from "./sections/homepage.js";
 import CreateAccount from "./sections/createAccount.js";
 import Schedules from "./sections/schedules.js";
 import ChangePass from "./sections/ChangePass.js";
+import HomeChangePass from "./sections/homeChangePass.js";
 import { SectionID } from "./data/data.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import useToken from "./hooks/useToken.js";
@@ -17,12 +18,16 @@ function App() {
     <div className="wrapper">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<CreateAccount />} />
+          <Route path="/" element={<HomeChangePass />} />
           <Route
             path={`/${SectionID.CreateAccount}`}
             element={<CreateAccount />}
           />
           <Route path={`/${SectionID.ChangePass}`} element={<ChangePass />} />
+          <Route
+            path={`/${SectionID.ChangePass}`}
+            element={<HomeChangePass />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ packages/react-frontend/src/sections/homeChangePass.js
@@ -0,0 +1,69 @@
+//homechangepass.js
+import React from "react";
+import Button from "../components/Button.js";
+import backgroundImage from "../resources/calpolycampus.jpg";
+import BackHeader from "../components/BackHeader";
+import TextBox from "../components/TextBox.js";
+
+function HomeChangePass() {
+  return (
+    <div className="content-center text-center">
+      <BackHeader text="Change Password" />
+
+      {/* Background Image */}
+      <div
+        className="absolute top-0 right-0 bottom-0 left-0"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          opacity: 0.3,
+        }}
+      ></div>
+
+      {/* Create Account Heading */}
+      <div className="absolute top-[150px] left-1/2 transform -translate-x-1/2">
+        <p
+          style={{ color: "#154734" }}
+          className="font-sans text-center text-5xl font-bold"
+        >
+          Change Password
+        </p>
+      </div>
+
+      {/* Content Center */}
+      <div className="absolute bottom-[250px] left-1/2 transform -translate-x-1/2">
+        <form>
+          {/* Current Password box */}
+          <TextBox
+            text="Current Password "
+            type="password"
+            className="mx-auto w-3/4 mt-5 mb-3 w-80"
+          />
+
+          {/* New Password box */}
+          <TextBox
+            text="New Password"
+            type="password"
+            className="mx-auto w-3/4 mb-3 w-80"
+          />
+
+          {/* Confirm Password box */}
+          <TextBox
+            text="Confirm Password"
+            type="password"
+            className="mx-auto w-3/4 mb-12 w-80"
+          />
+
+          {/* Create Account Button */}
+          <Button
+            text="Change Password"
+            className="bg-[#003831] text-white font-bold py-2 px-24 shadow-xl rounded mt-10 w-80 whitespace-nowrap"
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default HomeChangePass;
