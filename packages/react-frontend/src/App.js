a159fd98723db3a59ce6213e19918fbdcfcc87da
Meyli Jaeger <mjaege02@calpoly.edu>
2024-02-29 10:34:02 -0800

    forgotpassword

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -3,22 +3,23 @@ import CreateAccount from "./sections/createAccount.js";
 import Schedules from "./sections/schedules.js";
 import ChangePass from "./sections/ChangePass.js";
 import HomeChangePass from "./sections/homeChangePass.js";
+import ForgotPassword from "./sections/forgotPassword.js";
 import { SectionID } from "./data/data.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import useToken from "./hooks/useToken.js";
 
 function App() {
-  // const { token, setToken } = useToken();
+  const { token, setToken } = useToken();
 
-  // if (!token) {
-  //   return <HomePage setToken={setToken} />;
-  // }
+  if (!token) {
+    return <HomePage setToken={setToken} />;
+  }
 
   return (
     <div className="wrapper">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomeChangePass />} />
+          <Route path="/" element={<ForgotPassword />} />
           <Route
             path={`/${SectionID.CreateAccount}`}
             element={<CreateAccount />}
@@ -27,6 +28,10 @@ function App() {
           <Route
             path={`/${SectionID.ChangePass}`}
             element={<HomeChangePass />}
+          />
+          <Route
+            path={`/${SectionID.ForgotPassword}`}
+            element={<ForgotPassword />}
           />
         </Routes>
       </BrowserRouter>
--- packages/react-frontend/src/sections/forgotPassword.js
+++ packages/react-frontend/src/sections/forgotPassword.js
@@ -4,7 +4,7 @@ import BackHeader from "../components/BackHeader";
 import Button from "../components/Button";
 import TextBox from "../components/TextBox";
 
-function CreateAccount() {
+function ForgotPassword() {
   return (
     <div className="relative h-screen">
       <BackHeader text="Create Account" href="/" />
@@ -26,53 +26,27 @@ function CreateAccount() {
           style={{ color: "#154734" }}
           className="font-sans text-center text-5xl font-bold"
         >
-          Create Account
+          Forgot Password
         </p>
       </div>
 
       {/* Instruction Paragraph */}
       <div className="absolute top-[280px] left-1/2 transform -translate-x-1/2 w-4/5">
         <p className="font-sans text-center text-1xl font-bold mb-5">
-          Please fill out the following information to create a PolyBuddy
-          account.
+          Enter the email address associated with your PolyBuddy account.
         </p>
       </div>
 
       {/* Content Center */}
-      <div className="absolute bottom-[110px] left-1/2 transform -translate-x-1/2">
+      <div className="absolute top-[370px] left-1/2 transform -translate-x-1/2">
         <form>
-          {/* Username box */}
-          <TextBox
-            text="Username"
-            type="text"
-            className="mx-auto w-3/4 mt-5 mb-3 w-80"
-          />
-
-          {/* Cal Poly Email */}
-          <TextBox
-            text="Cal Poly Email"
-            type="text"
-            className="mx-auto w-3/4 mb-3 w-80"
-          />
-
-          {/* Password box */}
-          <TextBox
-            text="Password"
-            type="password"
-            className="mx-auto w-3/4 mb-3 w-80"
-          />
-
-          {/* Confirm Password box */}
-          <TextBox
-            text="Confirm Password"
-            type="password"
-            className="mx-auto w-3/4 mb-12 w-80"
-          />
+          {/* Email */}
+          <TextBox text="Email" type="text" className="mx-auto w-3/4 w-80" />
 
           {/* Create Account Button */}
           <Button
-            text="Sign Up"
-            className="bg-[#003831] text-white font-bold py-2.5 px-24 shadow-xl rounded mt-8 w-80"
+            text="Send Recovery Email"
+            className="bg-[#003831] text-white font-bold py-2.5 w-full shadow-xl rounded mt-8 w-80"
           />
         </form>
       </div>
@@ -80,4 +54,4 @@ function CreateAccount() {
   );
 }
 
-export default CreateAccount;
+export default ForgotPassword;
