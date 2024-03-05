5f1f9e02cb6b2f497637874c774478c6aa366d83
Michael <mvmonte12@gmail.com>
2024-03-05 13:17:58 -0800

    Added profile page, routes off of navigation bar

--- packages/react-frontend/src/sections/ProfilePage.js
+++ packages/react-frontend/src/sections/ProfilePage.js
@@ -2,6 +2,7 @@ import LogoutHeader from "../components/LogoutHeader.js";
 import NavBar from "../components/NavBar.js";
 import React, { useState, useEffect } from "react";
 
+<<<<<<< HEAD
 function ProfilePage({ token }) {
   const [email, setEmail] = useState("");
 
@@ -16,6 +17,20 @@ function ProfilePage({ token }) {
       .then((data) => {
         setEmail(data.email);
       });
+=======
+
+function ProfilePage({token}) {
+  const [email, setEmail] = useState("");
+
+  console.log(JSON.parse(atob(token.split('.')[1])).username);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/users/username/" + JSON.parse(atob(token.split('.')[1])).username)
+    .then((res)=>res.json())
+    .then((data) => {
+      setEmail(data.email);
+    });
+>>>>>>> 10c94f8 (Added profile page, routes off of navigation bar)
   }, "");
 
   // console.log(handleFetch());
@@ -25,16 +40,32 @@ function ProfilePage({ token }) {
       <LogoutHeader text="Profile" />
 
       <div>
+<<<<<<< HEAD
         <p className="text-center">Username</p>
         <p className="bg-[#e5e7eb] px-1 rounded w-50 text-center shadow-lg h-10 mt-2">
           {JSON.parse(atob(token.split(".")[1])).username}
         </p>
         <p className="text-center">Email</p>
+=======
+        <p className="text-center">
+          Username
+        </p>
+        <p className="bg-[#e5e7eb] px-1 rounded w-50 text-center shadow-lg h-10 mt-2">
+          {JSON.parse(atob(token.split('.')[1])).username}
+        </p>
+        <p className="text-center">
+          Email
+        </p>
+>>>>>>> 10c94f8 (Added profile page, routes off of navigation bar)
         <p className="bg-[#e5e7eb] px-1 rounded w-50 text-center shadow-lg h-10 mt-2">
           {/* {fetch("http://localhost:8000/users/username/" + JSON.parse(atob(token.split('.')[1])).username)
           .then((res)=>res).json().email} */}
           {email}
         </p>
+<<<<<<< HEAD
+=======
+
+>>>>>>> 10c94f8 (Added profile page, routes off of navigation bar)
       </div>
 
       <NavBar activePage="profile" />
