211e2828ee85b6b13a16b8e22dcb9fab9652069a
Michael <mvmonte12@gmail.com>
2024-03-12 13:47:33 -0700

    changed Profile Page CSS

--- packages/react-frontend/src/sections/ProfilePage.js
+++ packages/react-frontend/src/sections/ProfilePage.js
@@ -23,28 +23,32 @@ function ProfilePage({ token }) {
 
   return (
     <>
-      <LogoutHeader text="Profile" />
+      <div className="content-center text-center">
+        <LogoutHeader text="Profile" className="text-center"/>
 
-      <div className="mx-auto w-3/4 mt-12 mb-8">
-        <p className="text-center">Username</p>
-        <p className="bg-[#e5e7eb] px-1 rounded w-full text-center shadow-lg h-10 mt-2">
-          {JSON.parse(atob(token.split(".")[1])).username}
-        </p>
-        <p className="text-center">Email</p>
-        <p className="bg-[#e5e7eb] px-1 rounded w-full text-center shadow-lg h-10 mt-2">
-          {email}
-        </p>
-        <button
-          aria-label="Change Password Button"
-          onClick={() => (window.location.href = `${SectionID.ChangePass}`)}
-        >
-          Change Password
-        </button>
-      </div>
+        <div className="mx-auto w-3/4 mt-12 mb-8">
+          <p className="font-bold">Username</p>
+          <p className="bg-[#e5e7eb] px-1 rounded w-full text-center shadow-lg h-10 mt-2 align-middle">
+            {JSON.parse(atob(token.split(".")[1])).username}
+          </p>
+          <p className="font-bold">Email</p>
+          <p className="bg-[#e5e7eb] px-1 rounded w-full text-center shadow-lg h-10 mt-2 align-middle">
+            {email}
+          </p>
+          <div className="text-blue-500 cursor-pointer self-end ml-44 mt-2 font-bold text-right">
+          <button
+            aria-label="Change Password Button" 
+            onClick={() => (window.location.href = `${SectionID.ChangePass}`)}
+          >
+            Change Password
+          </button>
+          </div>
+        </div>
 
-      <div className="fixed bottom-0 left-0 right-0">
-        <NavBar activePage="profile" />
-      </div>
+        <div className="fixed bottom-0 left-0 right-0">
+          <NavBar activePage="profile" />
+        </div>
+        </div>
     </>
   );
 }
